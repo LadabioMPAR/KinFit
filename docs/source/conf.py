53639012a9a9b33daee1427eff62a1554adc7a24
daniel-c-souza <119331502+daniel-c-souza@users.noreply.github.com>
2025-04-24 14:40:21 -0300

    docs4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,20 +17,13 @@ release = '0.1.0'
 # Extensions
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.mathjax',
-    'myst_parser'
+
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
-
-# Theme settings
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 html_static_path = ['_static']
 
 templates_path = ['_templates']
@@ -41,5 +34,5 @@ language = 'pt-br'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 html_static_path = ['_static']
